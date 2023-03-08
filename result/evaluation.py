5bcb478fb89095fb30f19e44d9998a3a931c6f11
grey wang <52481781+wyyccc@users.noreply.github.com>
2023-03-08 16:26:49 +0800

    Update evaluation.py

--- result/evaluation.py
+++ result/evaluation.py
@@ -17,6 +17,18 @@ tag2index = {
     "B-14": "政策倾向", "I-14": "政策倾向",
 }
 
+tag2index = {
+    "O": 0,
+    "B-6": 1, "I-6": 2, "E-6":3,
+    "B-7": 4, "I-7": 5, "E-7":6,
+    "B-8": 7, "I-8": 8, "E-8":9,
+    "B-9": 10, "I-9": 11, "E-9":12,
+    "B-11": 13, "I-11": 14, "E-11":15,
+    "B-12": 16, "I-12": 17, "E-12":18,
+    "B-13": 19, "I-13": 20, "E-13":21,
+    "B-14": 22, "I-14": 23, "E-14":24,
+}
+
 def read_data(csv_path):
     result = list(pd.read_csv(csv_path)['result'])
     for i in tqdm(range(len(result))):
@@ -36,11 +48,11 @@ def evaluation(pred, true):
     print('f1:', metrics.f1_score(true, pred, average="micro"))
     return 0
 
-def evaluation_entity(pred, true):
+def evaluation_entity(pred, true, type = 'BIO'):
     TP = 0
     FP = 0
     FN = 0
-    for i in tqdm(range(len(pred))):
+    for i in range(len(pred)):
         temp_pred = pred[i]
         temp_true = true[i]
         tp = 0
@@ -51,15 +63,28 @@ def evaluation_entity(pred, true):
                 gt += 1
         for j in range(len(temp_pred)):
             if temp_pred[j][0] == 'B':
+                if temp_true[j] != temp_pred[j]:
+                    fp += 1
+                    continue
                 temp = True
-                for k in range(j, len(temp_pred)):
-                    if temp_pred[k] == 'O':
-                        if temp_true[k] != 'O':
+                if type == 'BIO':
+                    for k in range(j, len(temp_pred)):
+                        if temp_pred[k][0] != 'I':
+                            if temp_true[k][0] == 'I':
+                                temp = False
+                            break
+                        if temp_pred[k] != temp_true[k]:
                             temp = False
-                        break
-                    if temp_pred[k] != temp_true[k]:
-                        temp = False
-                        break
+                            break
+                elif type == 'BIOE':
+                    for k in range(j, len(temp_pred)):
+                        if temp_pred[k][0] == 'E':
+                            if temp_true[k] != temp_pred[k]:
+                                temp = False
+                            break
+                        if temp_pred[k] != temp_true[k]:
+                            temp = False
+                            break
                 if temp:
                     tp += 1
                 else:
@@ -68,8 +93,14 @@ def evaluation_entity(pred, true):
         TP += tp
         FP += fp
         FN += fn
-    precision = TP/(TP+FP)
-    recall = TP/(TP+FN)
+    if TP+FP == 0:
+        precision = 0
+    else:
+        precision = TP/(TP+FP)
+    if TP+FN == 0:
+        recall = 0
+    else:
+        recall = TP/(TP+FN)
     if precision == 0 and recall == 0:
         f1 = 0
     else:
@@ -77,60 +108,13 @@ def evaluation_entity(pred, true):
     print('precison:', precision)
     print('recall', recall)
     print('f1:', f1)
-    return 0
+    return f1
 
 
 if __name__ == '__main__':
-    csv_path = '/mnt/disk2/wyc/ner/result/bert-bilstm-crf-v3.csv'
+    type = 'BIOE'
+    csv_path = '/mnt/disk2/wyc/ner/bert-bilstm-crf/result/csv/bert-bilstm-crf-v6.csv'
     pred = read_data(csv_path)
-    data, true = data_eval()
-    evaluation(pred, true)
-    evaluation_entity(pred, true)
-    
-    '''
-    v1: bert-base-chinese / baseline / epoch 10 / lr 1e-5 -- 1e-6
-    precison: 0.7853338197738052    recall 0.7843227749708511    f1: 0.7848279717560128
-           O        0.98      0.98      0.98     1063275
-          人名       0.98      0.98      0.98      3074
-          会议       0.87      0.83      0.85      5447
-          地名       0.94      0.87      0.90     11864
-        政策倾向      0.74      0.76      0.75     36115
-        政策词汇      0.83      0.65      0.73      8303
-          时间       0.97      0.94      0.96     20975
-        核心观点      0.97      0.98      0.97     25122
-        组织机构      0.93      0.92      0.93     88824
-        accuracy                          0.97   1262999
-       macro avg      0.91      0.88      0.89   1262999
-    weighted avg      0.97      0.97      0.97   1262999
-        
-    v2: bert-base-chinese / linear lr & Lookahead & FGM / epoch 5 / lr 2e-6 -- 2e-5 -- 2e-6
-    precison: 0.8042144177449169    recall 0.7926301982122037    f1: 0.7983802894437444
-           O        0.98      0.98      0.98     1063275
-          人名       0.98      0.98      0.98      3074
-          会议       0.86      0.90      0.88      5447
-          地名       0.92      0.90      0.91     11864
-        政策倾向      0.76      0.74      0.75     36115
-        政策词汇      0.84      0.66      0.74      8303
-          时间       0.98      0.93      0.95     20975
-        核心观点      0.97      0.99      0.98     25122
-        组织机构      0.92      0.95      0.94     88824
-        accuracy                         0.97    1262999
-       macro avg      0.91      0.89      0.90   1262999
-    weighted avg      0.97      0.97      0.97   1262999
-    
-    v3: bert-base-chinese / linear lr & Lookahead & FGM & BERT 4-layer / epoch 8 / lr 2e-6 -- 2e-5 -- 2e-6
-    precison: 0.7952060856876301    recall 0.8074718227749709    f1: 0.8012920176929288
-           O         0.98      0.98      0.98   1063275
-          人名       0.98      0.99      0.98      3074
-          会议       0.86      0.85      0.86      5447
-          地名       0.93      0.89      0.91     11864
-        政策倾向      0.75      0.75      0.75     36115
-        政策词汇      0.82      0.72      0.76      8303
-          时间       0.97      0.95      0.96     20975
-        核心观点      0.96      0.99      0.98     25122
-        组织机构      0.93      0.94      0.94     88824
-
-        accuracy                           0.97   1262999
-       macro avg       0.91      0.90      0.90   1262999
-      weighted avg     0.97      0.97      0.97   1262999
-    '''
+    data, true = data_eval(type = type)
+    # evaluation(pred, true)
+    evaluation_entity(pred, true, type = type)
